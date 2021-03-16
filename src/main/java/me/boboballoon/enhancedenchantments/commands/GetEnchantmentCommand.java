2794d4726b628072c633e85f457ce9dd49cb4a54
BoBoBalloon <61363161+BoBoBalloon@users.noreply.github.com>
2021-03-16 10:19:16 -0400

    improved tab completion and command as a whole

--- src/main/java/me/boboballoon/enhancedenchantments/commands/GetEnchantmentCommand.java
+++ src/main/java/me/boboballoon/enhancedenchantments/commands/GetEnchantmentCommand.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -25,27 +26,21 @@ public class GetEnchantmentCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(TextUtil.format("&r&cOnly players can execute this command!"));
+        if (!sender.hasPermission("enhancedenchantments.getenchantment")) {
+            sender.sendMessage(TextUtil.format("&r&cYou do not have permission to execute this command!"));
             return false;
         }
 
-        Player player = (Player) sender;
-
-        if (!player.hasPermission("enhancedenchantments.getenchantment")) {
-            player.sendMessage(TextUtil.format("&r&cYou do not have permission to execute this command!"));
-            return false;
-        }
-
-        if (args.length != 2) {
-            player.sendMessage(TextUtil.format("&r&cYou have entered improper arguments to execute this command!"));
+        if (args.length < 2) {
+            sender.sendMessage(TextUtil.format("&r&cYou have entered improper arguments to execute this command!"));
+            TextUtil.format("&r&c/getenchantment <enchantment_name> <level> <player>");
             return false;
         }
 
         Enchantment enchantment = EnhancedEnchantments.getInstance().getEnchantmentManager().getEnchantment(args[0].replace('_', ' '));
 
         if (enchantment == null) {
-            player.sendMessage(TextUtil.format("&r&cYou have entered an invalid enchantment!"));
+            sender.sendMessage(TextUtil.format("&r&cYou have entered an invalid enchantment!"));
             return false;
         }
 
@@ -53,15 +48,36 @@ public class GetEnchantmentCommand implements CommandExecutor, TabCompleter {
         try {
             level = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            player.sendMessage(TextUtil.format("&r&cYou have entered an invalid level!"));
+            sender.sendMessage(TextUtil.format("&r&cYou have entered an invalid level!"));
             return false;
         }
 
         EnchantedBook book = new EnchantedBook(enchantment, level);
 
+        Player player;
+
+        if (args.length >= 3) {
+            player = Bukkit.getPlayerExact(args[2]);
+        } else if (sender instanceof Player) {
+            player = (Player) sender;
+        } else {
+            sender.sendMessage(TextUtil.format("&r&cYou must enter a valid player!"));
+            sender.sendMessage(TextUtil.format("&r&c/getenchantment <enchantment_name> <level> <player>"));
+            return false;
+        }
+
+        if (player == null) {
+            sender.sendMessage(TextUtil.format("&r&cYou must enter a valid player!"));
+            return false;
+        }
+
         player.getInventory().addItem(book.getBook());
 
         player.sendMessage(TextUtil.format("&r&aYou have been given " + enchantment.getTier().getColor() + enchantment.getName() + "&r&a!"));
+
+        if (!(sender instanceof Player) || !((Player) sender).getUniqueId().equals(player.getUniqueId())) {
+            sender.sendMessage(TextUtil.format("&r&aYou have given " + player.getName() + " " + enchantment.getTier().getColor() + enchantment.getName() + "&r&a!"));
+        }
 
         return true;
     }
@@ -76,11 +92,16 @@ public class GetEnchantmentCommand implements CommandExecutor, TabCompleter {
             return Arrays.asList("1", "2", "3");
         }
 
-        return null;
+        if (args.length == 3) {
+            return null;
+        }
+
+        return Collections.emptyList();
     }
 
     private List<String> getEnchantmentNames(String arg) {
         List<String> enchantments = new ArrayList<>();
+
         for (Enchantment enchantment : EnhancedEnchantments.getInstance().getEnchantmentManager().getEnchantments()) {
             String name = enchantment.getName().replace(' ', '_');
 
@@ -91,6 +112,7 @@ public class GetEnchantmentCommand implements CommandExecutor, TabCompleter {
 
             enchantments.add(name);
         }
+
         return enchantments;
     }
 }
